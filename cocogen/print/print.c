408e058336f5c5683c7e2435c61706235170c32a
Ischa Abraham <contact@ischa.dev>
2022-05-09 15:32:57 +0200

    add print traversal for rule

--- cocogen/print/print.c
+++ cocogen/print/print.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-#include "ccngen/trav.h"
-#include "ccngen/ast.h"
 #include "ccn/dynamic_core.h"
 #include "ccn/phase_driver.h"
+#include "ccngen/ast.h"
+#include "ccngen/trav.h"
 #include "globals.h"
-
 
 extern int **reachability_matrix;
 
@@ -15,15 +14,13 @@ static int indent = 0;
 #define INDENT indent += 4
 #define UNINDENT indent -= 4
 
-static void PrintIndent()
-{
+static void PrintIndent() {
     for (int i = 0; i < indent; i++) {
         putchar(' ');
     }
 }
 
-node_st *doOpts(node_st *ast)
-{
+node_st *doOpts(node_st *ast) {
     printf("Opt: %d\n", opt_counter);
     if (opt_counter < 50) {
         CCNcycleNotify();
@@ -32,8 +29,7 @@ node_st *doOpts(node_st *ast)
     return ast;
 }
 
-node_st *PRTast(node_st *ast)
-{
+node_st *PRTast(node_st *ast) {
     if (!globals.show_ast) {
         return ast;
     }
@@ -52,8 +48,7 @@ node_st *PRTast(node_st *ast)
     return ast;
 }
 
-node_st *PRTiphase(node_st *node)
-{
+node_st *PRTiphase(node_st *node) {
     printf("Phase %s {\n", ID_ORIG(IPHASE_NAME(node)));
     INDENT;
     PrintIndent();
@@ -69,8 +64,7 @@ node_st *PRTiphase(node_st *node)
     return node;
 }
 
-node_st *PRTitraversal(node_st *node)
-{
+node_st *PRTitraversal(node_st *node) {
     printf("Trav %s {\n", ID_ORIG(ITRAVERSAL_NAME(node)));
     INDENT;
     PrintIndent();
@@ -88,31 +82,26 @@ node_st *PRTitraversal(node_st *node)
     return node;
 }
 
-node_st *PRTitravdata(node_st *node)
-{
+node_st *PRTitravdata(node_st *node) {
     PrintIndent();
     printf("Travdata %s\n", ID_ORIG(ITRAVDATA_NAME(node)));
     return node;
 }
 
-node_st *PRTipass(node_st *node)
-{
+node_st *PRTipass(node_st *node) {
     printf("Pass: %s\n", ID_ORIG(IPASS_NAME(node)));
     return node;
 }
 
-
-node_st *PRTiactions(node_st *node)
-{
+node_st *PRTiactions(node_st *node) {
     PrintIndent();
-    printf("%s: %d\n", ID_ORIG(IACTIONS_REFERENCE(node)), IACTIONS_ACTION_ID(node));
+    printf("%s: %d\n", ID_ORIG(IACTIONS_REFERENCE(node)),
+           IACTIONS_ACTION_ID(node));
     TRAVnext(node);
     return node;
 }
 
-
-node_st *PRTinode(node_st *node)
-{
+node_st *PRTinode(node_st *node) {
     printf("NODE: %s(%d) {\n", ID_ORIG(INODE_NAME(node)), INODE_INDEX(node));
     INDENT;
     PrintIndent();
@@ -129,34 +118,56 @@ node_st *PRTinode(node_st *node)
     UNINDENT;
     PrintIndent();
     printf("}\n");
+    PrintIndent();
+    printf("Rules {\n");
+    INDENT;
+    TRAVopt(INODE_IRULES(node));
+    UNINDENT;
+    PrintIndent();
+    printf("}\n");
     UNINDENT;
     printf("}\n");
     TRAVopt(INODE_NEXT(node));
     return node;
 }
 
-node_st *PRTchild(node_st *node)
-{
+node_st *PRTchild(node_st *node) {
     TRAVopt(CHILD_NAME(node));
     TRAVopt(CHILD_NEXT(node));
     return node;
 }
 
-node_st *PRTattribute(node_st *node)
-{
+node_st *PRTrule(node_st *node) {
+    PrintIndent();
+    printf("%s", RULE_PATTERN(node));
+    switch (RULE_TYPE(node)) {
+    case RT_NULL:
+    case RT_template:
+        printf("\n");
+        break;
+    case RT_rewrite:
+        printf(" -> ");
+        break;
+    case RT_map:
+        printf(" = ");
+        break;
+    }
+    if (RULE_RESULT(node) != NULL)
+        printf("%s\n", RULE_PATTERN(node));
+    TRAVopt(RULE_NEXT(node));
+    return node;
+}
+
+node_st *PRTattribute(node_st *node) {
     PrintIndent();
     printf("%s\n", ID_ORIG(ATTRIBUTE_NAME(node)));
     TRAVopt(ATTRIBUTE_NEXT(node));
     return node;
 }
 
-node_st *PRTsetreference(node_st *node)
-{
-    return node;
-}
+node_st *PRTsetreference(node_st *node) { return node; }
 
-node_st *PRTsetliteral(node_st *node)
-{
+node_st *PRTsetliteral(node_st *node) {
     if (SETLITERAL_REFERENCE(node)) {
         PrintIndent();
         printf("%s\n", ID_ORIG(SETLITERAL_REFERENCE(node)));
@@ -166,8 +177,7 @@ node_st *PRTsetliteral(node_st *node)
     return node;
 }
 
-node_st *PRTinodeset(node_st *node)
-{
+node_st *PRTinodeset(node_st *node) {
     printf("nodeset %s {\n", ID_ORIG(INODESET_NAME(node)));
     INDENT;
     TRAVopt(INODESET_EXPR(node));
@@ -178,32 +188,23 @@ node_st *PRTinodeset(node_st *node)
     return node;
 }
 
-node_st *PRTid(node_st *node)
-{
+node_st *PRTid(node_st *node) {
     PrintIndent();
     printf("%s\n", ID_ORIG(node));
     return node;
 }
 
-node_st *PRTsetoperation(node_st *node)
-{
-    return node;
-}
+node_st *PRTsetoperation(node_st *node) { return node; }
 
-node_st *PRTienum(node_st *node)
-{
-    return node;
-}
+node_st *PRTienum(node_st *node) { return node; }
 
-node_st *PRTste(node_st *node)
-{
+node_st *PRTste(node_st *node) {
     printf("STE: %s, %d\n", ID_ORIG(STE_KEY(node)), NODE_TYPE(STE_VALUE(node)));
     TRAVchildren(node);
     return node;
 }
 
-node_st *PRTilifetime(node_st *node)
-{
+node_st *PRTilifetime(node_st *node) {
     if (ILIFETIME_TYPE(node) == LT_mandatory) {
         printf("mandatory");
     } else {
@@ -219,8 +220,7 @@ node_st *PRTilifetime(node_st *node)
     return node;
 }
 
-node_st *PRTlifetime_range(node_st *node)
-{
+node_st *PRTlifetime_range(node_st *node) {
     printf("%d", LIFETIME_RANGE_ACTION_ID(node));
     return node;
 }
